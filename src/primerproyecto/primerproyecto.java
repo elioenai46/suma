50b1dbafa224d471bdd76d863701375b80345e12
elioenai <elioenai.2001@outlook.com>
2022-03-08 18:29:27 -0600

    primer commit

---
+++ src/primerproyecto/primerproyecto.java
@@ -0,0 +1,14 @@
+package primerproyecto;
+
+public class primerproyecto {
+	public static void main(String []args){
+		int numero1 = 8;
+		int numero2 = 10;
+		int resultado;
+		resultado = numero1+numero2;
+		System.out.println("La suma es " + numero1 + " + " + numero2 + " = " + resultado);
+		
+	}
+}
+
+
